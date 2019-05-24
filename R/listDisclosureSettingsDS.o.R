a1c519326376ed86c45dc3c1719031a4a933b6e7
Stuart Wheater <stuart.wheater@arjuna.com>
2019-05-24 13:13:45 +0100

    Added 'nfilter.noise' and 'nfilter.levels'

--- R/listDisclosureSettingsDS.o.R
+++ R/listDisclosureSettingsDS.o.R
@@ -3,7 +3,7 @@
 #' @description This serverside function is an aggregate function that is called by the
 #' ds.listDisclosureSettings
 #' @details For more details see the extensive header for ds.listDisclosureSettings
-#' @author Paul Burton
+#' @author Paul Burton, Demetris Avraam for DataSHIELD Development Team
 #' @export
 #'
 listDisclosureSettingsDS.o <- function(){
@@ -13,12 +13,14 @@ listDisclosureSettingsDS.o <- function(){
   nf.glm <- getOption("nfilter.glm")
   nf.string <- getOption("nfilter.string")
   nf.stringShort <- getOption("nfilter.stringShort")
-  nf.kNN <- getOption("nfilter.kNN")	
+  nf.kNN <- getOption("nfilter.kNN")
+  nf.levels <- getOption("nfilter.levels")
+  nf.noise <- getOption("nfilter.noise")
   nfilter.privacy.old <- getOption("datashield.privacyLevel")
-	
+
   return(list(nfilter.tab=nf.tab,nfilter.subset=nf.subset,nfilter.glm=nf.glm,nfilter.string=nf.string,
-	            nfilter.stringShort=nf.stringShort,nfilter.kNN=nf.kNN,nfilter.privacy.old=nfilter.privacy.old))
-				
+              nfilter.stringShort=nf.stringShort,nfilter.kNN=nf.kNN,nfilter.levels=nf.levels,
+              nfilter.noise=nf.noise,nfilter.privacy.old=nfilter.privacy.old))
 }
 #AGGREGATE FUNCTION
 # listDisclosureSettingsDS.o
