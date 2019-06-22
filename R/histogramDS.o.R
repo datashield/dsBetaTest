df140359460549c3f876c02ed864308ecb5cde68
Stuart Wheater <stuart.wheater@arjuna.com>
2019-06-22 11:17:03 +0100

    Removed extra spaces

--- R/histogramDS.o.R
+++ R/histogramDS.o.R
@@ -1,9 +1,9 @@
-#' 
+#'
 #' @title Computes a histogram of the input variable without plotting.
 #' @description This function produces the information required to plot
 #' a histogram. This is done without allowing for bins (cells) with number
 #' of counts less than the pre-specified disclosure control set for the minimum cell
-#' size of a table. If a bin has less counts than this threshold then their counts 
+#' size of a table. If a bin has less counts than this threshold then their counts
 #' and its density are replaced by a 0 value.
 #' @param xvect the numeric vector for which the histogram is desired.
 #' @param min a numeric, the lower limit of the distribution.
@@ -12,7 +12,7 @@
 #' @return a list with an object of class \code{histogram} and the number of invalid cells
 #' @export
 #' @author Amadou Gaye, Demetris Avraam for DataSHIELD Development Team
-#' 
+#'
 histogramDS.o <- function (xvect, num.breaks, method.indicator, k, noise){
 
   #############################################################
@@ -27,14 +27,14 @@ histogramDS.o <- function (xvect, num.breaks, method.indicator, k, noise){
   nfilter.noise <- as.numeric(thr$nfilter.noise)              #
   nfilter.levels <- as.numeric(thr$nfilter.levels)            #
   #############################################################
-  
+
   if (method.indicator==1){
-    
+
     # generate a number based on the input variable that will be used as a fixed random number
     # generator
     x.seed <- seedDS.o(xvect)
     set.seed(x.seed)
-    
+
     rr <- c(min(xvect, na.rm=TRUE), max(xvect, na.rm=TRUE))
     if(rr[1] < 0){ min <- rr[1] * stats::runif(1, 1.01, 1.05) }else{ min <- rr[1] * stats::runif(1, 0.95, 0.99) }
     if(rr[2] < 0){ max <- rr[2] * stats::runif(1, 0.95, 0.99) }else{ max <- rr[2] * stats::runif(1, 1.01, 1.05) }
@@ -47,69 +47,69 @@ histogramDS.o <- function (xvect, num.breaks, method.indicator, k, noise){
     }else{
       # breaks
       brks <- seq(from=min, to=max, by=(max-min)/num.breaks)
-    } 
-	
+    }
+
     # Get the histogram object
     histout <- graphics::hist(xvect, breaks=brks, plot=FALSE)
-  
+
     # Check if the counts in each 'bin' are more than the disclosure setting for the minimum size
     # of table's cells
     indx <- which(histout$counts > 0 & histout$counts < nfilter.tab)
-    
+
     # get the number of invalid cells
     invalidcells <- length(indx)
-    
+
     if(invalidcells > 0){
       # Replace the corresponding counts and densities by zeros
       histout$counts[indx] <- 0
-      histout$density[indx] <- 0   
+      histout$density[indx] <- 0
     }
   }
 
   if(method.indicator==2){
-    
-    # Load the RANN package to use the 'nn2' function that searches for the Nearest Neighbours  
+
+    # Load the RANN package to use the 'nn2' function that searches for the Nearest Neighbours
     library(RANN)
-    
+
     # Remove any missing values
     x <- stats::na.omit(xvect)
-    
+
     # Standardise the variable
     x.standardised <- (x-mean(x))/stats::sd(x)
-    
-    # Calculate the length of the variable after ommitting any NAs 
+
+    # Calculate the length of the variable after ommitting any NAs
     N.data <- length(x)
-    
-    # Check if k is integer and has a value greater than or equal to the pre-specified threshold 
+
+    # Check if k is integer and has a value greater than or equal to the pre-specified threshold
     # and less than or equal to the length of rows of data.complete minus the pre-specified threshold
     if(k < nfilter.kNN | k > (N.data - nfilter.kNN)){
       stop(paste0("k must be greater than or equal to ", nfilter.kNN, " and less than or equal to ", (N.data-nfilter.kNN), "."), call.=FALSE)
     }else{
       neighbours = k
     }
-    
-    # Find the k-1 nearest neighbours of each data point 
+
+    # Find the k-1 nearest neighbours of each data point
     nearest <- RANN::nn2(x.standardised, k = neighbours)
-    
-    # Calculate the centroid of each n nearest data points 
+
+    # Calculate the centroid of each n nearest data points
     x.centroid <- matrix()
     for (i in 1:N.data){
       x.centroid[i] <- mean(x.standardised[nearest$nn.idx[i,1:neighbours]])
     }
-    
+
     # Calculate the scaling factor
     x.scalingFactor <- stats::sd(x.standardised)/stats::sd(x.centroid)
-    
+
     # Apply the scaling factor to the centroids
     x.masked <- x.centroid * x.scalingFactor
-    
+
     # Shift the centroids back to the actual position and scale of the original data
     x.new <- (x.masked * stats::sd(x)) + mean(x)
-    
+
     # find the minimum and the maximum of the distribution
     min <- min(x.new)
     max <- max(x.new)
-    
+
     # Check if the number of breaks meets the DataSHIELD privacy criteria (disclosure control for
     # saturation)
     if (num.breaks > (nfilter.levels * length(xvect))){
@@ -118,21 +118,21 @@ histogramDS.o <- function (xvect, num.breaks, method.indicator, k, noise){
     }else{
       # breaks
       brks <- seq(from=min, to=max, by=(max-min)/num.breaks)
-    } 
-    
+    }
+
     # Get the histogram object
     histout <- graphics::hist(x.new, breaks=brks, plot=FALSE)
     invalidcells <- 0
   }
-  
+
   if(method.indicator==3){
-    
+
     # Remove any missing values
     x <- stats::na.omit(xvect)
-    
-    # Calculate the length of the variable after ommitting any NAs 
+
+    # Calculate the length of the variable after ommitting any NAs
     N.data <- length(x)
-    
+
     # Check if the percentage of the variance that is specified in the argument 'noise'
     # and is used as the variance of the embedded noise is a greater
     # than the minimum threshold specified in the filter 'nfilter.noise'
@@ -141,19 +141,19 @@ histogramDS.o <- function (xvect, num.breaks, method.indicator, k, noise){
     }else{
       percentage <- noise
     }
-    
+
     # generate a number based on the input variable that will be used as a fixed random number
     # generator
     x.seed <- seedDS.o(x)
     set.seed(x.seed)
-    
+
     # generate the noise-augmented vector
     x.new <- x + stats::rnorm(N.data, mean=0, sd=sqrt(percentage*stats::var(x)))
-    
+
     # find the minimum and the maximum of the distribution
     min <- min(x.new)
     max <- max(x.new)
-    
+
     # Check if the number of breaks meets the DataSHIELD privacy criteria (disclosure control for
     # saturation)
     if (num.breaks > (nfilter.levels * length(xvect))){
@@ -162,16 +162,16 @@ histogramDS.o <- function (xvect, num.breaks, method.indicator, k, noise){
     }else{
       # breaks
       brks <- seq(from=min, to=max, by=(max-min)/num.breaks)
-    } 
-    
+    }
+
     # Get the histogram object
     histout <- graphics::hist(x.new, breaks=brks, plot=FALSE)
     invalidcells <- 0
-  }  
-    
+  }
+
   # Return a list with the histogram object and the number of invalid cells
   return(list("histobject"=histout, "invalidcells"=invalidcells))
-  
+
 }
 # AGGREGATE FUNCTION
 # histogramDS.o
